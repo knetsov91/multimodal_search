17b62e4086f72dac097a978a19e0021db03d866a
Konstantin <knetsov91@gmail.com>
2026-06-11 15:07:49 +0300

    feat: Add SQLAlchemy engine and session factory

--- db/database.py
+++ db/database.py
@@ -7,3 +7,14 @@ POSTGRES_USERNAME = os.getenv("POSTGRES_USERNAME", "")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 DATABASE_URL = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/recipes"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+	db = SessionLocal()
+	try:
+		yield db
+	finally:
+		db.close()
