3d2c5a60fa754c993d8cec68110fb3e8aa6dd45c
Konstantin <knetsov91@gmail.com>
2026-06-12 11:39:35 +0300

    feat: Add recipe create and edit endpoints

--- endpoints/recipes.py
+++ endpoints/recipes.py
@@ -37,3 +37,25 @@ def recipe_edit(request: Request,
 	recipe = recipe_service.get_recipe_by_id(id=id)
 	img = minio_client_v2.generate_presigned_url(recipe[0]["img_name"])
 	return templates.TemplateResponse(name="recipe-edit.html", request=request, context={"session": request.session, "recipe": recipe[0], "img": img, "id": id})
+
+@router.patch("/api/v1/recipes/{id}")
+async def edit(
+		   response: Response,
+		   id: int,
+		   file: Optional[UploadFile] = File(None),
+		   recipe_text: Optional[str] = Form(""),
+		   recipe_title: Optional[str] = Form("")):
+	await recipe_service.save_recipe(file, recipe_title, recipe_text, id)
+	return RedirectResponse(url="/admin", status_code=303)
+@router.post("/api/v1/recipes")
+async def upload(
+		   response: Response,
+		   file: UploadFile = File(),
+		   recipe_text: Optional[str] = Form(""),
+		   recipe_title: Optional[str] = Form("")):
+
+	try:
+		await recipe_service.save_recipe(file, recipe_title, recipe_text)
+	except Exception as e:
+		raise HTTPException(status_code=400, detail=str(e))
+	return RedirectResponse(url="/admin", status_code=303)
